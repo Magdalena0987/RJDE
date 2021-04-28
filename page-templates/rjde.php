ac00346527ebfc1c465eaabf670ae5263f9b40d6
Magdalena0987 <magdalena@biorangemedia.co.uk>
2021-04-28 17:27:04 +0100

    homepage-code

--- page-templates/rjde.php
+++ page-templates/rjde.php
@@ -11,22 +11,35 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<div class="two-col js-animate-fade-low js-animate-delay-1">
-			<div class="col col-one">
-				<h2 class="">Unlock the true potential of your business</h2>
-				<div class="btn"></div>
-			</div>
-			<div class="col col-two">
-				<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes" class="lettings-2">
+        <div class="two-col-wrapper">
+			<div class="two-col js-animate-fade-low js-animate-delay-1">
+				<div class="col col-one">
+					<h2 class="">Unlock the true potential of your business</h2>
+					<div class="btn">CONTACT</div>
+				</div>
+				<div class="col col-two">
+					<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/key-lock.png" alt="key lock" class="lettings-2">
+				</div>
 			</div>
 		</div>
 
 		<div class="four-col">
-            <div><h2>Strategie</h2></div>
-			<div><h2>Master Data Management</h2></div>
-			<div><h2>Consultancy</h2></div>
-			<div><h2>Training</h2></div>
+            <div>
+				<h2>Strategie</h2>
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/noun_strategy_2499109-1.svg" alt="strategy icon" class="lettings-2">
+		    </div>
+			<div>
+				<h2>Master Data Management</h2>
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/noun_marketing_3154527-1.svg" alt="marketing icon" class="lettings-2">
+			</div>
+			<div>
+				<h2>Consultancy</h2>
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/noun_Communication_905835-1.svg" alt="communication icon" class="lettings-2">
+			</div>
+			<div>
+				<h2>Training</h2>
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/noun_training_1894962-1.svg" alt="training icon" class="lettings-2">
+			</div>
 		</div>
 
 		<div class="title-center">
@@ -99,22 +112,23 @@ get_header();
   </div> 
 
     <div class="partners-wrapper">
-		<div class="partners">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
+	    <img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/rjde-background.png" alt="training icon" class="lettings-2">
+		<div class="partners one">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/rijksoverheid.svg" alt="rijksoverheid logo" class="rijksoverheid">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/Gecon-1.svg" alt="gecon logo" class="gecon">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/Nestle.svg" alt="nestle logo" class="nestle">
 		</div>
 
-		<div class="partners">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
+		<div class="partners two">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/Innotractor-1.svg" alt="Innotractor logo" class="Innotractor">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/sec_airdesign-1.svg" alt="sec_airdesign" class="sec_airdesign">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/Prospectations_rgb-1.svg" alt="Prospectations" class="Prospectations">
 		</div>
 
-		<div class="partners">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
-			<img src="/wp-content/uploads/2021/03/Kent-countryside-village.jpg" alt="moving house boxes">
+		<div class="partners three">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/2018_BOM_Branding_Logo-1.svg" alt="bom logo" class="bom">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/violanta_logo_en-2.svg" alt="violanta logo" class="violanta">
+			<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/Logo-R-Gin-1.svg" alt="Logo-R-Gin logo" class="Logo-R-Gin">
 		</div>
     </div>
 	<footer>
