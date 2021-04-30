46f8aeaeb5dbf6cd9ba46e534e1b89b4fb49aa60
Magdalena0987 <magdalena@biorangemedia.co.uk>
2021-04-30 15:13:33 +0100

    about us finished

--- page-templates/about.php
+++ page-templates/about.php
@@ -14,40 +14,56 @@ get_header();
 
 		<div class="members">
             <div>
-				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/efraim.svg" alt="Efraim" class="lettings-2">
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/efraim.png" alt="Efraim" class="">
 				<h2>Efraim – Business strategie</h2>
+				<div class="social-links">
+					<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/LinkedIN_black.svg" alt="Efraim" class="linkedin">
+					<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/email_24px.svg" alt="Efraim" class="">
+				</div>
 		    </div>
 			<div>
-				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/joe.svg" alt="Joe" class="lettings-2">
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/joe.png" alt="Joe" class="lettings-2">
 				<h2>Joe - Marketing strategie</h2>
+				<div class="social-links">
+				    <img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/LinkedIN_black.svg" alt="Efraim" class="linkedin">
+					<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/email_24px.svg" alt="Efraim" class="">
+				</div>
 			</div>
 			<div>
-				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/pascal.svg" alt="Pascal" class="lettings-2">
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/pascal.png" alt="Pascal" class="">
 				<h2>Pascal – Multilevel acquisitie</h2>
+				<div class="social-links">
+				    <img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/LinkedIN_black.svg" alt="Efraim" class="linkedin">
+					<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/email_24px.svg" alt="Efraim" class="">
+				</div>
 			</div>
 			<div>
-				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/frank.svg" alt="Frank" class="lettings-2">
+				<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/frank.png" alt="Frank" class="">
 				<h2>Frank - Master data management</h2>
+				<div class="social-links">
+				    <img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/LinkedIN_black.svg" alt="Efraim" class="linkedin">
+					<img src="http://rjde.bigorangeclients.co.uk/wp-content/uploads/2021/04/email_24px.svg" alt="Efraim" class="">
+				</div>
 			</div>
 		</div>
 
-	<div class="footer-about">
-		<div class="col col-one">
-			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="">Services</a></li>
-				<li><a href="">Packages</a></li>
-				<li><a href="">Contact Us</a></li>
-			</ul>
+		<div class="footer-about">
+			<div class="col col-one">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Services</a></li>
+					<li><a href="">Packages</a></li>
+					<li><a href="">Contact Us</a></li>
+				</ul>
+			</div>
+			<div class="col col-two">
+				<ul>
+					<li><a href="">Terms & Conditions</a></li>
+					<li><a href="">Privacy policy</a></li>
+					<li><a href="">Cookie Policy</a></li>
+				</ul>
+			</div>
 		</div>
-		<div class="col col-two">
-			<ul>
-				<li><a href="">Terms & Conditions</a></li>
-				<li><a href="">Privacy policy</a></li>
-				<li><a href="">Cookie Policy</a></li>
-			</ul>
-		</div>
-	</div>
 
 	</main>
 <?php
